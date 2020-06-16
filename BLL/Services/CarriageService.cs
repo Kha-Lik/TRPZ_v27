285d23bf1b85688d75e8480189a922fd0c727428
MykolaV <kolya.vasevich@gmail.com>
2020-06-16 07:57:28 +0300

    created BLL

---
+++ BLL/Services/CarriageService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using BLL.Models;
+
+namespace BLL.Services
+{
+    public class CarriageService : ICarriageService
+    {
+
+        public ICollection<Carriage> GetSpecifiedCarriages(CarriageClass carriageClass, Train train)
+        {
+            var carriages = train.Carriages.Where(c => c.Class.Equals(carriageClass));
+            return carriages as ICollection<Carriage>;
+        }
+
+        public Carriage GetCarriageByNum(Train train, int num)
+        {
+            return train.Carriages.First(c => c.Number == num);
+        }
+    }
+}
---
+++ BLL/Services/TicketService.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using AutoMapper;
+using BLL.Models;
+using DAL;
+using DAL.Entities;
+using DAL.Interfaces;
+
+namespace BLL.Services
+{
+    public class TicketService : ITicketService
+    {
+        private IInMemoryStorage _inMemoryStorage;
+        private ITrainService _trainService;
+        private ICarriageService _carriageService;
+        private IMapper _mapper;
+
+        public TicketService(IInMemoryStorage inMemoryStorage, IMapper mapper, ITrainService trainService, ICarriageService carriageService)
+        {
+            _inMemoryStorage = inMemoryStorage;
+            _mapper = mapper;
+            _trainService = trainService;
+            _carriageService = carriageService;
+        }
+
+        public Ticket TakeTicket(Ticket ticket)
+        {
+            if (_inMemoryStorage.Tickets.LastOrDefault() != null)
+                // ReSharper disable once PossibleNullReferenceException
+                ticket.Number = ++_inMemoryStorage.Tickets.LastOrDefault().Number;
+            else
+                ticket.Number = 1;
+
+            var seat = _carriageService.GetCarriageByNum(_trainService.GetTrainByNum(ticket.TrainNumber),
+                ticket.CarriageNumber).Seats.First(s => s.Number == ticket.Seat);
+            seat.IsTaken = true;
+
+            var ticketEntity = _mapper.Map<TicketEntity>(ticket);
+            _inMemoryStorage.Tickets.Add(ticketEntity);
+            return ticket;
+        }
+    }
+}
