6ad1c1a8ab27ed65c1b0baba645d55d8c44d3a81
MykolaV <kolya.vasevich@gmail.com>
2020-06-24 22:16:08 +0300

    replaced serialization with database via ef core

---
+++ DAL/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using DAL.Entities;
+
+namespace DAL.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        IQueryable<TEntity> GetAll();
+
+        TEntity GetById(int id);
+        
+        void Create(TEntity entity);
+        
+        void Update(TEntity entity);
+        
+        void Delete(TEntity entity);
+    }
+}
---
+++ DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Entities;
+
+namespace DAL.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        public IRepository<TrainEntity> TrainRepository { get; }
+        
+        public IRepository<TicketEntity> TicketRepository { get; }
+
+        void Save();
+    }
+}
